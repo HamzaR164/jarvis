93666b260e0fcf1547669879d1105cbbe0c080eb
HamzaR164 <hamzarahmiabdulghani@gmail.com>
2026-07-25 04:25:53 +0300

    Clean rebuild: preload.js and index.html without unexplained content

--- desktop/preload.js
+++ desktop/preload.js
@@ -7,6 +7,7 @@ contextBridge.exposeInMainWorld('jarvis', {
   systemStats: () => ipcRenderer.invoke('get-system-stats'),
   transcribe: (base64Audio) => ipcRenderer.invoke('transcribe', base64Audio),
   notifyState: (state) => ipcRenderer.send('state-changed', state),
+  getScreenSource: () => ipcRenderer.invoke('get-screen-source'),
   launchApp: (name) => ipcRenderer.invoke('launch-app', name),
   openExternal: (url) => ipcRenderer.invoke('open-external', url),
   saveRecording: (base64Data) => ipcRenderer.invoke('save-recording', base64Data),
